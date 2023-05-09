efc7b6d0d6d13758ec6cae82b7c9cad82a5253a8
Alexandre <alexsabbah23@outlook.com>
2023-05-09 08:59:27 -0300

    fourth commit

--- lib/view/tela_de_afazeres_view.dart
+++ lib/view/tela_de_afazeres_view.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:projeto_aula/widgets/todowid.dart';
-
 import '../model/tarefas.dart';
 
 class TodoListView extends StatefulWidget {
@@ -19,69 +18,108 @@ class _TodoListViewState extends State<TodoListView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Future.delayed(Duration.zero, () {
-                Navigator.pop(context);
-              });
-            },
-          ),
-          title: Text('Tarefas'),
-          centerTitle: true,
-          backgroundColor: Color(0xFF6495ED),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Future.delayed(Duration.zero, () {
+              Navigator.pop(context);
+            });
+          },
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-          child: Column(
-            children: [
-              Row(children: [
-                Expanded(
-                  flex: 10,
-                  child: TextField(
-                    controller: txtTarefa,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Adicione uma tarefa",
-                        hintText: "Comprar pão amanhã às 6"),
-                  ),
-                ),
-                SizedBox(width: 7),
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        String texto = txtTarefa.text;
-                        setState(() {
-                          Tarefas newTarefa =
-                              Tarefas(titulo: texto, dateTime: DateTime.now());
-                          anotacoes.add(newTarefa);
-                        });
-                        txtTarefa.clear();
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF6495ED),
-                          padding: EdgeInsets.all(23.2)),
-                      child: Icon(
-                        Icons.add,
-                        size: 20,
-                      )),
-                ),
-              ]),
-              SizedBox(
-                height: 20,
-              ),
+        title: Text('Tarefas'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF6495ED),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Column(
+          children: [
+            Row(children: [
               Expanded(
-                child: ListView(
-                  children: [
-                    for (Tarefas anotacao in anotacoes)
-                      novaTarefa(titulo: anotacao.titulo),
-                  ],
+                flex: 10,
+                child: TextField(
+                  controller: txtTarefa,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Adicione uma tarefa",
+                      hintText: "Comprar pão amanhã às 6"),
                 ),
-              )
-            ],
-          ),
-        ));
+              ),
+              SizedBox(width: 7),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: () {
+                      String texto = txtTarefa.text;
+                      setState(() {
+                        Tarefas newTarefa =
+                            Tarefas(titulo: texto, dateTime: DateTime.now());
+                        anotacoes.add(newTarefa);
+                      });
+                      txtTarefa.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF6495ED),
+                        padding: EdgeInsets.all(23.2)),
+                    child: Icon(
+                      Icons.add,
+                      size: 20,
+                    )),
+              ),
+            ]),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  for (Tarefas anotacao in anotacoes)
+                    novaTarefa(titulo: anotacao.titulo),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                apagarTodasTarefas();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF6495ED),
+                padding: EdgeInsets.all(23.2),
+              ),
+              child: Text("Apagar tudo"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void apagarTodasTarefas() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Tem Certeza?"),
+        content: Text("Todas as tarefas criadas serão apagadas"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancelar")),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  anotacoes.clear();
+                });
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red,
+              ),
+              child: Text("Sim"))
+        ],
+      ),
+    );
   }
 }
